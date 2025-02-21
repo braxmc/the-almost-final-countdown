c63f5ef61304269ab376d2904dc350daf9fc8fad
Braxton McClellan <thebraxmc@gmail.com>
2025-02-20 23:59:18 -0700

    adding challenge areas to the project

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,33 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+
+  const [ name, setName ] = useState(null);
+  // const [ submitted, setSubmitted ] = useState(false);
+
+  // function handleNameChange(event) {
+  //   setSubmitted(false);
+  //   setName(event.target.value);
+  // }
+
+  function handleClick() {
+    // setSubmitted(true);
+    setName(playerName.current.value);
+    playerName.current.value = '';
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { name ?? 'unknown entity' }</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input 
+          ref={playerName} 
+          type="text" 
+          // onChange={handleNameChange} 
+          // value={name}
+        />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
